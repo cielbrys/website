dc8c7e2f4f55c8a85fc9cfe5ef02d6ccbfc2c3c7
cielbrys <cielbrys@gmail.com>
2019-10-15 16:16:55 +0200

    updaye webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, {mode}) => {
   console.log(mode);
   return {
     output: {
-      path: `${__dirname}/public`,
+      path: `${__dirname  }/public`,
       filename: '[name].[hash].js'
     },
     devServer: {
